6cfab5e9d5638d27b614161f572622ba0d821398
DAZUN XING <dazun.xing@gmail.com>
2020-05-16 08:30:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,14 +32,8 @@ class Song
   
   def self.find_by_name(name)
     song =self.new
-    # song =self.all.find {|x| x.name==name} ? song : false
-    self.all.each do |x|
-      if x.name==name
-        return x
-      elsif self.all.find {|y| y.name==name}==nil
-        return false 
-      end
-    end
+    self.all.find {|x| x.name==name} 
+ 
   end
 
 end
