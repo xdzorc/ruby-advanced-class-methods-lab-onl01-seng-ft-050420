08cea204ad37ee08120683ff21838397e24b463c
DAZUN XING <dazun.xing@gmail.com>
2020-05-16 09:46:32 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,60 @@ class Song
   def save
     self.class.all << self
   end
-
+  
+  def self.create
+    song=self.new
+    song.save
+    song
+  end
+  
+  
+  def self.new_by_name(name)
+    song =self.new 
+    song.name =name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song =self.new
+    song.name =name
+    song.save
+    song
+  end
+  
+  def self.find_by_name(name)
+    song =self.new
+    self.all.find {|x| x.name==name} 
+  end
+  
+  def self.find_or_create_by_name(name)
+    
+    if self.find_by_name(name)==nil 
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+     
+  def self.alphabetical  
+    self.all.sort_by {|x| x.name}
+  end
+  
+  def self.new_from_filename(name)
+    song =self.new 
+    array =name.split(/[-.]/)
+    song.name =array[1].split(" ").join(" ") 
+    song.artist_name =array[0].split(" ").join(" ") 
+    song
+  end
+  
+  def self.create_from_filename(name)
+    song=self.new
+    song=self.new_from_filename(name)
+    song.save
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 end
